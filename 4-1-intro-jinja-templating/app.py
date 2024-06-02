75aeaceab0da84da2f75a5175a6a9340a21bda36
nata-ferguson <nata.ferguson@outlook.com>
2024-06-02 15:03:16 -0500

    added another example

--- 4-1-intro-jinja-templating/app.py
+++ 4-1-intro-jinja-templating/app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     user = {'username': 'Natasha'}
-    return render_template('index.html', title='Home', user=user)
+    current_time = datetime.now().hour
+    greeting = "Good morning" if current_time < 12 else "Good afternoon" if current_time < 18 else "Good evening" 
+    return render_template('index.html', title='Home', user=user, greeting=greeting)
 
 if __name__ == '__main__':
     app.run(debug=True)
