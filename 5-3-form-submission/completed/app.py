4ba9289e2cba2d9944ed31a026c77b5560d828b9
nata-ferguson <nata.ferguson@outlook.com>
2024-06-25 23:14:36 -0500

    added code for lesson 5-3, updated 5-2

--- 5-3-form-submission/completed/app.py
+++ 5-3-form-submission/completed/app.py
@@ -2,16 +2,17 @@ from flask import Flask, render_template, request
 from forms import MyForm
 
 app = Flask(__name__)
-app.secret_key = 'supersecretkey'
+app.secret_key = 'supersecretkey'  # Necessary for CSRF protection
 
 @app.route('/form', methods=['GET', 'POST'])
 def form():
     form = MyForm()
-    if form.validate_on_submit():
+    if request.method == 'POST' and form.validate_on_submit():
         name = form.name.data
         email = form.email.data
-        return f"Hello, {name}! We’ve received your email: {email}."
+        return render_template('success.html', name=name, email=email)
     return render_template('form.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
