3b776f83498cf8be330e46dddfc7d66cc814d2d3
nata-ferguson <nata.ferguson@outlook.com>
2024-08-09 10:05:33 -0500

    minor modifications to examples

--- 4-1-intro-jinja-templating/starter/app.py
+++ 4-1-intro-jinja-templating/starter/app.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    user = {'username': 'Natasha'}
-    return render_template('index.html', title='Welcome', user=user)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
